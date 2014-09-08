829dc610029116239220bd50d5ef2bae5aa27c72
mukunda- <mukunda@mukunda.com>
2014-09-08 10:51:01 -0600

    new process.bat

--- gfx/process.bat
+++ gfx/process.bat
@@ -1,6 +1,18 @@
 
 @echo Processing: %1 ......
 
+@set valid=true
+@if "%1" == "" set valid=false
+@if "%2" == "" set valid=false
+@if "%3" == "" set valid=false
+
+@if "%valid%" == "false" (
+	@echo "Usage: process.bat <directory> <width> <height> [-x]"
+	@echo   directory - path to directory containing list of sprite files to compile.
+	@echo   width,height - dimensions of the images, in pixels.
+	@echo   -x - sets translucent flag for material
+	@goto skipcompile
+)
 
 @echo -----------------------------------
 @echo creating vtf...
@@ -14,14 +26,21 @@ genqc.py -g %VGNAME% -m %1 -s compile.smd -o compile.qc
 @echo generating SMD...
 @echo -----------------------------------
 gensmd.py --width=%2 --height=%3 --texture=%1 --output=compile.smd
-rem gensmd.py --width=1 --height=1 --texture=%1 --output=compile.smd
+@echo -----------------------------------
+@echo generating material...
+@echo -----------------------------------
+genvmt.py -g %VGNAME% -t %1 -o produce\%1.vmt %4
+
+@if "%GAMEDIR%" == "" (
+	@echo Please specify a GAMEDIR to compile the graphic.
+	@goto skipcompile
+) 
+
 @echo -----------------------------------
 @echo compiling...
 @echo -----------------------------------
 "%GAMEDIR%\..\bin\studiomdl" -game "%GAMEDIR%" compile.qc
-@echo -----------------------------------
-@echo generating material...
-genvmt.py -g %VGNAME% -t %1 -o produce\%1.vmt %4
+
 @echo -----------------------------------
 @echo copying material...
 @echo -----------------------------------
@@ -30,3 +49,4 @@ copy produce\%1.vmt "%GAMEDIR%\materials\videogames\%VGNAME%\" /Y
 @echo -----------------------------------
 @echo done.
 @echo -----------------------------------
+skipcompile:
